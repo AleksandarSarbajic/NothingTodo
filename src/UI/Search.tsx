9131350ac41f15c45c28d7f7a0d6bb0d9b2229c3
Aleksandar <sleasarbajic@gmail.com>
2023-11-22 21:00:06 +0100

    Fixed error

--- src/UI/Search.tsx
+++ src/UI/Search.tsx
@@ -1,7 +1,7 @@
 import { HiOutlineMagnifyingGlass } from "react-icons/hi2";
-import styled from "styled-components";
+// import styled from "styled-components";
 
-const StyledSearch = styled.div``;
+// const StyledSearch = styled.div``;
 
 function Search() {
   return <HiOutlineMagnifyingGlass />;
